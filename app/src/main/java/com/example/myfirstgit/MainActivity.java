fd931921bb56e3c6dce3e3fdde406c9d6290c3f9
OhMiYoung <theand0811@naver.com>
2021-07-07 10:14:21 +0900

    토스트 메시지 추가

--- app/src/main/java/com/example/myfirstgit/MainActivity.java
+++ app/src/main/java/com/example/myfirstgit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myfirstgit;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "안녕하세요", Toast.LENGTH_SHORT).show();
     }
 }
